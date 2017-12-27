6d1683cd24cec47019fc8e82013b720f27e2a491
Lucas Borio <lucas@arzion.com>
2017-12-27 15:00:04 -0300

    expense tags

--- application/models/ExpenseTag.php
+++ application/models/ExpenseTag.php
@@ -4,7 +4,7 @@ class ExpenseTag extends ActiveRecord\Model{
     
     static $belongs_to = array(
         array('building', 'readonly' => true),
-        array('expense_balance_tags', 'readonly' => true)
+        array('expense_balance_tag', 'readonly' => true)
     );
     
 }
